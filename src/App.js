82c9f4f74b1564cca516eb00a22cdd445e7943ca
longdh <longdh@imediatech.com.vn>
2022-08-24 20:33:09 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import Welcome from "./components/Welcome";
+import Clothes from "./components/Clothes";
+export default function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Welcome />
+      <Clothes name="Quần jean" type="Skinny" color="Đen" size="L">
+        Clothes 1
+      </Clothes>
+      <Clothes name="Váy" type="váy công chúa" color="Trắng" size="M">
+        Clothes 2
+      </Clothes>
     </div>
   );
 }
-
-export default App;
